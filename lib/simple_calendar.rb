a70c77f6e6efc3d899c48aeaf343169118618f9c
Chris Oliver <excid3@gmail.com>
2012-02-20 10:51:03 -0600

    No need to check for Rails constant, this requires Rails.

--- lib/simple_calendar.rb
+++ lib/simple_calendar.rb
@@ -1,4 +1,4 @@
 require "simple_calendar/version"
 require "simple_calendar/view_helpers"
 require "simple_calendar/model_additions"
-require "simple_calendar/railtie" if defined?(Rails)
+require "simple_calendar/railtie"
