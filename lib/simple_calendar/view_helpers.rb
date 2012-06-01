65288fb86169680fa95f1fe45c72b13b5c39a3e1
Josh Chernoff <jchernoff@gfxcomplex.com>
2012-06-01 00:10:33 -0500

    rewrote the whole draw for the calendar, still need to added month header and events block

--- lib/simple_calendar/view_helpers.rb
+++ lib/simple_calendar/view_helpers.rb
@@ -1,85 +1,70 @@
 module SimpleCalendar
   module ViewHelpers
+
     def calendar(events, &block)
-      day = Date.civil((params[:year] || Time.zone.now.year).to_i, (params[:month] || Time.zone.now.month).to_i)
+      selected_month = Date.civil((params[:year] || Time.zone.now.year).to_i, (params[:month] || Time.zone.now.month).to_i)
+      current_date   = Date.today  
+      range          = build_range selected_month
+      month_array    = build_month range
 
-      content_tag :table, :class => "bordered-table calendar" do
-        month_header(day) + day_header + body(day, events, block)
+      draw_calendar(selected_month, month_array, current_date)
+    end
+
+    private
+
+    def build_range(selected_month)
+      start_date     = selected_month.beginning_of_month
+      start_date     = start_date.sunday? ? start_date : start_date.beginning_of_week.advance(:days => -1)      
+      end_date       = selected_month.end_of_month
+      end_date       = end_date.sunday? ? end_date.advance(:days => 1).end_of_week : end_date      
+      date_range     = (start_date..end_date).to_a   
+    end 
+
+    def build_month(date_range)
+      month          = []
+      week           = []
+      i              = 0
+      date_range.each do |date|         
+        week << date          
+        if i == 6
+          i = 0
+          month << week
+          week = []
+        else
+          i += 1 
+        end
       end
+      return month
     end
 
-    def start_date(date)
-      start_date = date.beginning_of_month
-      start_date.beginning_of_week.advance(:days => -1) unless start_date.sunday?
-    end
+    def draw_calendar(selected_month, month, current_date)
+      tags = []
 
-    def end_date(date)
-      end_date = date.end_of_month
-      end_date.sunday? ? end_date.advance(:days => 1).end_of_week : end_date
-    end
+      content_tag(:table) do
 
-    def month_header(day)
-      content_tag :h2 do
-        previous_month = day.advance :months => -1
-        next_month = day.advance :months => 1
-        tags = []
-        tags << link_to("<", request.fullpath.split('?').first + "?month=#{previous_month.month}&year=#{previous_month.year}")
-        tags << day.strftime("%B %Y")
-        tags << link_to(">", request.fullpath.split('?').first + "?month=#{next_month.month}&year=#{next_month.year}")
+        tags << content_tag(:thead, content_tag(:tr, I18n.t("date.abbr_day_names").collect { |name| content_tag :th, name}.join.html_safe))
+
+        tags << content_tag(:tbody) do 
+
+          month.collect do |week| 
+
+            content_tag(:tr, :class => "week") do
+
+              week.collect do |date| 
+
+                content_tag(:td, :class => "day") do
+
+                  content_tag(:div, :class => (Date.today == current_date ? "today" : nil)) do
+                    date.day.to_s
+                  end #content_tag :div
+
+                end #content_tag :td
+              end.join.html_safe #week.collect
+            end #content_tag :tr
+          end.join.html_safe #month.collect
+        end #content_tag :tbody 
         tags.join.html_safe
-      end
-    end
-
-    def day_header
-      content_tag :thead do
-        content_tag :tr do
-          I18n.t(:"date.abbr_day_names").map{ |day| content_tag :th, day }.join.html_safe
-        end
-      end
-    end
-
-    def body(day, events, block)
-      current_date = start_date(day).dup
-
-      content_tag :tbody do
-        weeks = []
-        while current_date < end_date(day)
-          weeks << content_tag(:tr) do
-            tags = []
-            while not current_date.saturday?
-              tags << day(current_date, day, events, block)
-              current_date = current_date.tomorrow
-            end
-            tags << day(current_date, day, events, block)
-            current_date = current_date.tomorrow
-            tags.join.html_safe
-          end
-        end
-        weeks.join.html_safe
-      end
-    end
-
-    def day(date, selected_month,  events, block)
-
-      is_today = (Date.today == date ? "today " : "")
-      is_not_selected_month = (selected_month.month != date.month ? "not_selected_month " : "")
-      date_class = is_today + is_not_selected_month
-
-      if date_class.empty?
-        date_class = nil
-      end
-
-      content_tag :td, :class => date_class do
-        concat content_tag(:div, date.day, :class => "day")
-
-        day_events(date, events).map do |event|
-          block.call(event)
-        end.join.html_safe
-      end
-    end
-
-    def day_events(date, events)
-      events.select { |e| e.start_time_column.to_date == date }
-    end
+      end #content_tag :table
+    end #draw_calendar
   end
 end
