f8b622789c299f3d68613c10c984d822bc191c0c
Josh Chernoff <jchernoff@gfxcomplex.com>
2012-06-01 13:47:21 -0700

    typo

--- lib/simple_calendar/view_helpers.rb
+++ lib/simple_calendar/view_helpers.rb
@@ -96,7 +96,7 @@ module SimpleCalendar
             previous_month = selected_month.advance :months => -1
             next_month = selected_month.advance :months => 1
             tags = []
-            tags << link_to("&laquo;".html_safe, request.fullpath.split('?').first + "?month=#{previous_month.month}&year=#{previous_month.year}", :class => "previus-month")
+            tags << link_to("&laquo;".html_safe, request.fullpath.split('?').first + "?month=#{previous_month.month}&year=#{previous_month.year}", :class => "previous-month")
             tags << selected_month.strftime("%B %Y")
             tags << link_to("&raquo;".html_safe, request.fullpath.split('?').first + "?month=#{next_month.month}&year=#{next_month.year}",         :class => "next-month")
             tags.join.html_safe
