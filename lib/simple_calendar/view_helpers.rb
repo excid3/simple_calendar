4170829031b73073e43a84788c514d45be587c23
Chris Oliver <excid3@gmail.com>
2012-06-11 15:48:38 -0500

    Cleaned up code visually a bit

--- lib/simple_calendar/view_helpers.rb
+++ lib/simple_calendar/view_helpers.rb
@@ -3,7 +3,7 @@ module SimpleCalendar
 
     def calendar(events, &block)
       selected_month = Date.civil((params[:year] || Time.zone.now.year).to_i, (params[:month] || Time.zone.now.month).to_i)
-      current_date   = Date.today  
+      current_date   = Date.today
       range          = build_range selected_month
       month_array    = build_month range
 
@@ -13,94 +13,95 @@ module SimpleCalendar
     private
 
     def build_range(selected_month)
-      start_date     = selected_month.beginning_of_month
-      start_date     = start_date.sunday? ? start_date : start_date.beginning_of_week.advance(:days => -1)
+      start_date = selected_month.beginning_of_month
+      start_date = start_date.sunday? ? start_date : start_date.beginning_of_week.advance(:days => -1)
 
-      end_date       = selected_month.end_of_month
-      end_date       = end_date.sunday? ? end_date : end_date.advance(:days => 1).end_of_week 
+      end_date   = selected_month.end_of_month
+      end_date   = end_date.sunday? ? end_date : end_date.advance(:days => 1).end_of_week
 
-      date_range     = (start_date..end_date).to_a   
-
-    end 
+      date_range = (start_date..end_date).to_a
+    end
 
     def build_month(date_range)
-      month          = []
-      week           = []
-      i              = 0
-      date_range.each do |date|         
-        week << date   
+      month = []
+      week  = []
+      i     = 0
+
+      date_range.each do |date|
+        week << date
         if i == 6
           i = 0
           month << week
-
           week = []
         else
-          i += 1 
+          i += 1
         end
       end
-      return month
+
+      month
     end
 
+    # Renders the calendar table
     def draw_calendar(selected_month, month, current_date, events, block)
-
       tags = []
 
       content_tag(:table, :class => "table table-bordered table-striped calendar") do
-        
         tags << month_header(selected_month)
-
         tags << content_tag(:thead, content_tag(:tr, I18n.t("date.abbr_day_names").collect { |name| content_tag :th, name, :class => (selected_month.month == Date.today.month && Date.today.strftime("%a") == name ? "current-day" : nil)}.join.html_safe))
+        tags << content_tag(:tbody) do
 
-        tags << content_tag(:tbody) do 
+          month.collect do |week|
+            content_tag(:tr, :class => (week.include?(Date.today) ? "current-week week" : "week")) do
 
-          month.collect do |week| 
-
-            content_tag(:tr, :class => (week.include?(Date.today) ? "current-week week" : "week")) do              
-
-              week.collect do |date| 
-
+              week.collect do |date|
                 tb_class = []
-
                 tb_class << not_current_month = (date.month == selected_month.month ? "" : "not-currnet-month")
-
                 tb_class << (Date.today == date ? "today day" : "day")
 
                 content_tag(:td, :class => tb_class.join(" ")) do
-
                   content_tag(:div) do
-
                     divs = []
 
                     concat content_tag(:div, date.day.to_s)
-                    
                     divs << day_events(date, events).collect {|event| block.call(event) }
-
                     divs.join.html_safe
-
                   end #content_tag :div
                 end #content_tag :td
+
               end.join.html_safe
             end #content_tag :tr
+
           end.join.html_safe
-        end #content_tag :tbody 
+        end #content_tag :tbody
+
         tags.join.html_safe
       end #content_tag :table
-    end #draw_calendar
+    end
 
+    # Returns an array of events for a given day
     def day_events(date, events)
       events.select { |e| e.start_time_column.to_date == date }
     end
 
+    # Generates the header that includes the month and next and previous months
     def month_header(selected_month)
-          content_tag :h2 do
-            previous_month = selected_month.advance :months => -1
-            next_month = selected_month.advance :months => 1
-            tags = []
-            tags << link_to("&laquo;".html_safe, request.fullpath.split('?').first + "?month=#{previous_month.month}&year=#{previous_month.year}", :class => "previous-month")
-            tags << selected_month.strftime("%B %Y")
-            tags << link_to("&raquo;".html_safe, request.fullpath.split('?').first + "?month=#{next_month.month}&year=#{next_month.year}",         :class => "next-month")
-            tags.join.html_safe
-          end
-        end
+      content_tag :h2 do
+        previous_month = selected_month.advance :months => -1
+        next_month = selected_month.advance :months => 1
+        tags = []
+
+        tags << link_to("&laquo;".html_safe, "#{simple_calendar_path}?month=#{previous_month.month}&year=#{previous_month.year}", :class => "previous-month")
+        tags << selected_month.strftime("%B %Y")
+        tags << link_to("&raquo;".html_safe, "#{simple_calendar_path}?month=#{next_month.month}&year=#{next_month.year}",         :class => "next-month")
+
+        tags.join.html_safe
+      end
+    end
+
+    # Returns the full path to the calendar
+    # This is used for generating the links to the next and previous months
+    def simple_calendar_path
+      request.full_path.split('?').first
+    end
   end
 end
