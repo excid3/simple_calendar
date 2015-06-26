d5118e3d36980bef9f27e98b710ca318088dfee9
Chris Oliver <excid3@gmail.com>
2015-06-26 15:19:47 -0400

    Remove require tests

--- spec/simple_calendar_spec.rb
+++ spec/simple_calendar_spec.rb
@@ -2,16 +2,7 @@ require 'spec_helper'
 require 'rails'
 
 describe SimpleCalendar do
-    it 'has a version number' do
-        expect(SimpleCalendar::VERSION).not_to be nil
-    end
-    
-    it 'requires simple_calendar' do
-        expect(require 'simple_calendar').not_to be nil
-    end
-    
-    it 'requires Rails' do
-        expect(require 'rails').not_to be nil
-    end
-    
+  it 'has a version number' do
+    expect(SimpleCalendar::VERSION).not_to be nil
+  end
 end
