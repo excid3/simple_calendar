fb862f57affe942605959e49ecc2b60af3ee7336
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-07-13 23:27:04 +0000

    Add SimpleCalendar::Generators::ViewsGenerator spec

---
+++ spec/views_generators_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+require "generators/simple_calendar/views_generator"
+
+describe SimpleCalendar::Generators::ViewsGenerator do
+    it 'copies the files to app/views/simple_calendar'
+    it 'verifies the content'
+end
