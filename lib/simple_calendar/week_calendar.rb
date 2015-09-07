60f2b7d9f49af70e6b6a768bd959f12089915481
Chris Oliver <excid3@gmail.com>
2015-09-07 14:27:39 -0400

    Allow customizing number of weeks in the week calendar

--- lib/simple_calendar/week_calendar.rb
+++ lib/simple_calendar/week_calendar.rb
@@ -3,7 +3,14 @@ module SimpleCalendar
     private
 
       def date_range
-        (start_date.beginning_of_week..start_date.end_of_week).to_a
+        starting = start_date.beginning_of_week
+        ending = (starting + (number_of_weeks - 1).weeks).end_of_week
+
+        (starting..ending).to_a
+      end
+
+      def number_of_weeks
+        options.fetch(:number_of_weeks, 1)
       end
   end
 end
