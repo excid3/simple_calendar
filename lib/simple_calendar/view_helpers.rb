6a7d22b41ca97204ab29da63f58d5e0acfebbfe5
Chris Oliver <excid3@gmail.com>
2015-09-07 14:03:16 -0400

    Update view helpers for new calendars

--- lib/simple_calendar/view_helpers.rb
+++ lib/simple_calendar/view_helpers.rb
@@ -2,17 +2,17 @@ module SimpleCalendar
   module ViewHelpers
     def calendar(options={}, &block)
       raise 'calendar requires a block' unless block_given?
-      SimpleCalendar::Calendar.new(self, options).render(block)
+      SimpleCalendar::Calendar.new(self, options).render(&block)
     end
 
     def month_calendar(options={}, &block)
       raise 'month_calendar requires a block' unless block_given?
-      SimpleCalendar::MonthCalendar.new(self, options).render(block)
+      SimpleCalendar::MonthCalendar.new(self, options).render(&block)
     end
 
     def week_calendar(options={}, &block)
       raise 'week_calendar requires a block' unless block_given?
-      SimpleCalendar::WeekCalendar.new(self, options).render(block)
+      SimpleCalendar::WeekCalendar.new(self, options).render(&block)
     end
   end
 end
