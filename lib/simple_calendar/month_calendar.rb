921fa00adaccdb04156a4f569dea47674a2739d9
Arman Adhitama Prawirasasra <black.biron@gmail.com>
2016-09-06 23:48:43 +0700

    fix bug on weekstart

--- lib/simple_calendar/month_calendar.rb
+++ lib/simple_calendar/month_calendar.rb
@@ -3,7 +3,7 @@ module SimpleCalendar
     private
 
       def date_range
-        (start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week).to_a
+        (start_date.beginning_of_month.at_beginning_of_week..start_date.end_of_month.at_end_of_week).to_a
       end
   end
 end
