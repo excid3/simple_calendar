0236b370080a038e2aaa3d1ab2837c7c6be8a283
Alex Graves <graves@thedp.com>
2017-08-27 17:31:03 -0400

    test current-week class

--- spec/views/simple_calendar/_calendar.html.erb_spec.rb
+++ spec/views/simple_calendar/_calendar.html.erb_spec.rb
@@ -2,16 +2,11 @@ require 'spec_helper'
 require 'action_controller'
 require 'simple_calendar/calendar'
 
-describe 'simple_calendar/_calendar.html.erb', type: :view do
+describe '_calendar.html.erb', type: :view do
+  let(:calendar) { SimpleCalendar::Calendar.new(ViewContext.new) }
+
   it 'should have the current week' do
-    # assign(:calendar, SimpleCalendar::Calendar.new(ViewContext.new))
-
-    # render template: 'simple_calendar/_calendar.html.erb'
-    render
-    expect(rendered).to have_css 'tr.current-week'
+    week = calendar.send(:date_range)
+    expect(calendar.send(:tr_classes_for, week)).to include('current-week')
   end
-
-  # it {
-  #   should have_css('div.simple-calendar')
-  # }
 end
