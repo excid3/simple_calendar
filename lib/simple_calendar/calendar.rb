ba98ced772c71be27b219a95beeaa522b6c3a06c
John Dell <john@mutations.ltd>
2020-06-24 15:47:39 -0700

    Format link date in ISO8601 format
    * If the default date format has been changed from default (ISO8601), prev/next link will not be valid.
    
    Signed-off-by: John Dell <john@mutations.ltd>

--- lib/simple_calendar/calendar.rb
+++ lib/simple_calendar/calendar.rb
@@ -58,11 +58,11 @@ module SimpleCalendar
     end
 
     def url_for_next_view
-      view_context.url_for(@params.merge(start_date_param => date_range.last + 1.day))
+      view_context.url_for(@params.merge(start_date_param => (date_range.last + 1.day).iso8601))
     end
 
     def url_for_previous_view
-      view_context.url_for(@params.merge(start_date_param => date_range.first - 1.day))
+      view_context.url_for(@params.merge(start_date_param => (date_range.first - 1.day).iso8601))
     end
 
     def date_range
