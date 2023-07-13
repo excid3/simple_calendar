6458158bcb26b8742ec6b9b188a5a839f32db420
Chris Oliver <excid3@gmail.com>
2023-07-13 13:36:47 -0500

    Rename railtie file to engine since we're using an engine and autoload constants

--- lib/simple_calendar.rb
+++ lib/simple_calendar.rb
@@ -1,6 +1,13 @@
+require "simple_calendar/engine"
 require "simple_calendar/calendar"
 require "simple_calendar/month_calendar"
 require "simple_calendar/week_calendar"
-require "simple_calendar/railtie"
 require "simple_calendar/version"
 require "simple_calendar/view_helpers"
+
+module SimpleCalendar
+  autoload :Calendar, "simple_calendar/calendar"
+  autoload :MonthCalendar, "simple_calendar/month_calendar"
+  autoload :WeekCalendar, "simple_calendar/week_calendar"
+  autoload :ViewHelpers, "simple_calendar/view_helpers"
+end
--- lib/simple_calendar/railtie.rb
+++
@@ -1,9 +0,0 @@
-module SimpleCalendar
-  class Engine < Rails::Engine
-    initializer "simple_calendar.view_helpers" do
-      ActiveSupport.on_load(:action_view) do
-        include ViewHelpers
-      end
-    end
-  end
-end
