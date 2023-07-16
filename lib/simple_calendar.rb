b63a6f037f9c30ec1883bebc965996ad12f87d08
Xavier Noria <fxn@hashref.com>
2023-07-16 19:07:05 +0200

    Simplify entrypoint

--- lib/simple_calendar.rb
+++ lib/simple_calendar.rb
@@ -1,13 +1,8 @@
-require "simple_calendar/engine"
-require "simple_calendar/calendar"
-require "simple_calendar/month_calendar"
-require "simple_calendar/week_calendar"
-require "simple_calendar/version"
-require "simple_calendar/view_helpers"
-
 module SimpleCalendar
-  autoload :Calendar, "simple_calendar/calendar"
-  autoload :MonthCalendar, "simple_calendar/month_calendar"
-  autoload :WeekCalendar, "simple_calendar/week_calendar"
-  autoload :ViewHelpers, "simple_calendar/view_helpers"
+  require_relative "simple_calendar/engine"
+  require_relative "simple_calendar/calendar"
+  require_relative "simple_calendar/month_calendar"
+  require_relative "simple_calendar/week_calendar"
+  require_relative "simple_calendar/version"
+  require_relative "simple_calendar/view_helpers"
 end
