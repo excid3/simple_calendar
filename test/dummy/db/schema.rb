1f0a6fbffe8a00241100687a3b34358c8d089791
Chris Oliver <excid3@gmail.com>
2023-08-08 09:19:12 -0500

    Use schema 6.1 for tests

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_184313) do
+ActiveRecord::Schema[6.1].define(version: 2022_09_30_184313) do
   create_table "meetings", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time", precision: nil
