7840aebd73c9ee8092794fcc95d1228d0316801f
Chris Oliver <excid3@gmail.com>
2024-04-01 13:43:48 -0500

    Update test helper fixtures for Rails 7.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,12 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/mi
 require "rails/test_help"
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  ActiveSupport::TestCase.fixture_paths << File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_paths << File.expand_path("../fixtures", __FILE__)
+elsif ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
 end
+
+ActiveSupport::TestCase.fixtures :all
